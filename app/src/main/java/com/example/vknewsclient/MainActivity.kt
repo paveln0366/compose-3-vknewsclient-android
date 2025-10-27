fbb45a5c78f3ee65c0f6912cd1245a69bcb11f6c
Pavel Potapov <potapov@goozix.com>
2025-10-27 18:53:24 +0300

    Add lesson 27

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -8,9 +8,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,4 +43,38 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+private fun Test() {
+    Box(Modifier.fillMaxSize(), Alignment.Center) {
+        Example3()
+    }
+}
+
+@Composable
+private fun Example1() {
+    OutlinedButton({}) {
+        Text("Hello World")
+    }
+}
+
+@Composable
+private fun Example2() {
+    TextField(
+        value = "Value",
+        onValueChange = {},
+        label = { Text("Label") }
+    )
+}
+
+@Composable
+private fun Example3() {
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = { Text("Yes", Modifier.padding(8.dp)) },
+        title = { Text("Are your sure?") },
+        text = { Text("Do you want to delete this file?") },
+        dismissButton = { Text("No", Modifier.padding(8.dp)) }
+    )
 }
